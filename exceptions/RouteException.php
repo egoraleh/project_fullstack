e355b3ef750349e310e45a8de8b62d08fcef0c79
egoraleh <yegorzaleh2104@gmail.com>
2025-04-23 10:26:49 +0300

    Added route exception.

--- exceptions/RouteException.php
+++ exceptions/RouteException.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 namespace app\exceptions;
 
+use Throwable;
+
 class RouteException extends \Exception
 {
+    private string $path;
 
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null, string $path = "")
+    {
+        parent::__construct($message, $code, $previous);
+        $this->path = $path;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
 }
